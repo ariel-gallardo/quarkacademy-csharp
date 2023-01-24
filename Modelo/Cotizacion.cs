f090c06c72e61db7d9316a55f6fd254841650934
ariel-gallardo <ariel.gallardo@wirsolut.com>
2023-01-24 20:05:08 -0300

    parte-a# Vendedor - Cotizacion csharp

---
+++ Modelo/Cotizacion.cs
@@ -0,0 +1,12 @@
+﻿namespace Modelo
+{
+    public class Cotizacion
+    {
+        public string Identificacion { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string CodigoVendedor { get; set; }
+        public string CodigoPrenda { get; set; }
+        public int Unidades { get; set; }
+        public double Calculo { get; set; }
+    }
+}
